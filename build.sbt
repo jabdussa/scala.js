a097baf8f60e2d90c138df0d76b273abb3036675
Anwar Abdus-Samad <jabdussa@gmail.com>
2016-10-31 13:45:53 -0400

    Mon Oct 31 13:45:53 EDT 2016

--- build.sbt
+++ build.sbt
@@ -1,21 +1,22 @@
+// Turn this project into a Scala.js project by importing these settings
 import com.lihaoyi.workbench.Plugin._
 
 enablePlugins(ScalaJSPlugin)
 
 workbenchSettings
 
+scalaVersion := "2.11.4"
+
 name := "Example"
 
 version := "0.1-SNAPSHOT"
 
-scalaVersion := "2.11.7"
-
 libraryDependencies ++= Seq(
-  "org.scala-js" %%% "scalajs-dom" % "0.8.2",
-  "com.lihaoyi" %%% "scalatags" % "0.5.4"
+  "org.scala-js" %%% "scalajs-dom" % "0.8.0",
+  "com.lihaoyi" %%% "scalatags" % "0.4.5",
+  "com.lihaoyi" %%% "scalarx" % "0.2.7"
 )
 
-bootSnippet := "example.ScalaJSExample().main(document.getElementById('canvas'));"
+bootSnippet := "example.ScalaJSExample().main()"
 
 updateBrowsers <<= updateBrowsers.triggeredBy(fastOptJS in Compile)
-
--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -1,44 +1,146 @@
 package example
-import scala.scalajs.js.annotation.JSExport
+
 import org.scalajs.dom
-import org.scalajs.dom.html
-import scala.util.Random
+import scalatags.JsDom.all._
 
-case class Point(x: Int, y: Int){
-  def +(p: Point) = Point(x + p.x, y + p.y)
-  def /(d: Int) = Point(x / d, y / d)
-}
+import scalatags.JsDom.tags2.section
+import rx._
+import scala.scalajs.js.annotation.JSExport
+import scala.Some
 
+
+case class Task(txt: Var[String], done: Var[Boolean])
 @JSExport
 object ScalaJSExample {
+  import Framework._
+
+  val editing = Var[Option[Task]](None)
+
+  val tasks = Var(
+    Seq(
+      Task(Var("TodoMVC Task A"), Var(true)),
+      Task(Var("TodoMVC Task B"), Var(false)),
+      Task(Var("TodoMVC Task C"), Var(false))
+    )
+  )
+
+  val filter = Var("All")
+
+  val filters = Map[String, Task => Boolean](
+    ("All", t => true),
+    ("Active", !_.done()),
+    ("Completed", _.done())
+  )
+
+  val done = Rx{tasks().count(_.done())}
+
+  val notDone = Rx{tasks().length - done()}
+
+  val inputBox = input(
+    id:="new-todo",
+    placeholder:="What needs to be done?",
+    autofocus:=true
+  ).render
+
   @JSExport
-  def main(canvas: html.Canvas): Unit = {
-    val ctx = canvas.getContext("2d")
-                    .asInstanceOf[dom.CanvasRenderingContext2D]
+  def main(): Unit = {
+    dom.document.body.innerHTML = ""
+    dom.document.body.appendChild(
+      section(id:="todoapp")(
+        header(id:="header")(
+          h1("todos"),
+          form(
+            inputBox,
+            onsubmit := { () =>
+              tasks() = Task(Var(inputBox.value), Var(false)) +: tasks()
+              inputBox.value = ""
+              false
+            }
+          )
+        ),
+        section(id:="main")(
+          input(
+            id:="toggle-all",
+            `type`:="checkbox",
+            cursor:="pointer",
+            onclick := { () =>
+              val target = tasks().exists(_.done() == false)
+              Var.set(tasks().map(_.done -> target): _*)
+            }
+          ),
+          label(`for`:="toggle-all", "Mark all as complete"),
+          Rx {
+            ul(id := "todo-list")(
+              for (task <- tasks() if filters(filter())(task)) yield {
+                val inputRef = input(`class` := "edit", value := task.txt()).render
 
-    var count = 0
-    var p = Point(0, 0)
-    val corners = Seq(Point(255, 255), Point(0, 255), Point(128, 0))
+                li(
+                  `class` := Rx{
+                    if (task.done()) "completed"
+                    else if (editing() == Some(task)) "editing"
+                    else ""
+                  },
+                  div(`class` := "view")(
+                    ondblclick := { () =>
+                      editing() = Some(task)
+                    },
+                    input(
+                      `class` := "toggle",
+                      `type` := "checkbox",
+                      cursor := "pointer",
+                      onchange := { () =>
+                        task.done() = !task.done()
+                      },
+                      if (task.done()) checked := true
+                    ),
+                    label(task.txt()),
+                    button(
+                      `class` := "destroy",
+                      cursor := "pointer",
+                      onclick := { () =>tasks() = tasks().filter(_ != task) }
+                    )
+                  ),
+                  form(
+                    onsubmit := { () =>
+                      task.txt() = inputRef.value
+                      editing() = None
+                      false
+                    },
+                    inputRef
+                  )
+                )
+              }
+            )
+          },
+          footer(id:="footer")(
+            span(id:="todo-count")(strong(notDone), " item left"),
 
-    def clear() = {
-      ctx.fillStyle = "black"
-      ctx.fillRect(0, 0, 255, 255)
-    }
-
-    def run = for (i <- 0 until 10){
-      if (count % 3000 == 0) clear()
-      count += 1
-      p = (p + corners(Random.nextInt(3))) / 2
-
-      val height = 512.0 / (255 + p.y)
-      val r = (p.x * height).toInt
-      val g = ((255-p.x) * height).toInt
-      val b = p.y
-      ctx.fillStyle = s"rgb($g, $r, $b)"
-
-      ctx.fillRect(p.x, p.y, 1, 1)
-    }
-
-    dom.setInterval(() => run, 50)
+            ul(id:="filters")(
+              for ((name, pred) <- filters.toSeq) yield {
+                li(a(
+                  `class`:=Rx{
+                    if(name == filter()) "selected"
+                    else ""
+                  },
+                  name,
+                  href:="#",
+                  onclick := {() => filter() = name}
+                ))
+              }
+            ),
+            button(
+              id:="clear-completed",
+              onclick := { () => tasks() = tasks().filter(!_.done()) },
+              "Clear completed (", done, ")"
+            )
+          )
+        ),
+        footer(id:="info")(
+          p("Double-click to edit a todo"),
+          p(a(href:="https://github.com/lihaoyi/workbench-example-app/blob/todomvc/src/main/scala/example/ScalaJSExample.scala")("Source Code")),
+          p("Created by ", a(href:="http://github.com/lihaoyi")("Li Haoyi"))
+        )
+      ).render
+    )
   }
 }
